a436808cdad080f1f4672aa1ec2f4d16eaf021e6
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2021-08-16 19:43:24 -0300

    Projeto finalizado v1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var seconds = 1857;
+var seconds = 1670;
 
 setTimeout(() => {
-    document.querySelector('.botao').classList.remove('disabled');
+    document.querySelector('#botao').style.display='inline-block';
     document.querySelector('.cartoes').style.display = 'inline-flex';
   }, seconds*1000);
