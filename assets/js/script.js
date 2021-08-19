61522259754de39194e7d273ba846ee7f2235a90
Pedro Figueiredo <pedrohfm2012@yahoo.com.br>
2021-08-19 15:15:01 -0300

    v1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-var seconds = 1670;
+var seconds = 1654;
+var delay = 1694;
 
 setTimeout(() => {
     document.querySelector('#botao').style.display='inline-block';
     document.querySelector('.cartoes').style.display = 'inline-flex';
   }, seconds*1000);
+
+setTimeout(()=> {
+    document.querySelector('.sound').style.display='none';
+}, delay *1000)
